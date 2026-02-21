702ef92360e8c5e5f6a4ee3fa2d0b06f9d3b8394
peter-perger <pergerpeti2@gmail.com>
2026-02-21 11:56:36 +0100

    Uploaded code to GitHub

---
+++ main.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+from datetime import datetime
+import streamlit as st
+import requests
+import os
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
+base_url = os.getenv("URL").replace("DEMO_KEY", api_key)
+
+
+def generate_pic(selected_date):
+    params = {
+        "api_key": api_key,
+        "date": selected_date.strftime("%Y-%m-%d")
+    }
+
+    response = requests.get(base_url, params=params)
+    print(response.status_code)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        st.header(data.get('title', 'Astronomy Picture'))
+        img_url = data.get('hdurl', data.get('url'))
+
+        if data.get("media_type") == "image":
+            st.image(img_url)
+        else:
+            st.video(img_url)
+
+        st.write(data.get('explanation'))
+
+    else:
+        st.error(f"Error {response.status_code}: Could not fetch data.")
+
+
+st.subheader('Picture of the day')
+user_date = st.date_input(label="🚀🧑‍🚀")
+
+if user_date:
+    generate_pic(user_date)
